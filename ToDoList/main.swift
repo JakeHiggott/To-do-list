77adaea5acc3f716ad655a43d043e3f9a1e67b83
Jake Higgot <jakehiggott1@gmail.com>
2018-02-21 13:47:30 -0500

    Initial Commit

---
+++ ToDoList/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ToDoList
+//
+//  Created by Jake Higgot on 2/21/18.
+//  Copyright © 2018 Jake Higgott. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
